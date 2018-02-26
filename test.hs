fa72fe0331ca9285cb4d86532fb9a7f25e16eedc
vieirat <vieirat@mcmaster.ca>
2018-02-25 22:35:43 -0500

    adding test files

---
+++ test.hs
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf600
+{\fonttbl\f0\fnil\fcharset0 Georgia;}
+{\colortbl;\red255\green255\blue255;\red27\green29\blue31;}
+{\*\expandedcolortbl;\csgray\c100000;\cssrgb\c14118\c15294\c16078;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl520\partightenfactor0
+
+\f0\fs33\fsmilli16650 \cf2 \expnd0\expndtw0\kerning0
+Oniwnvlenownvoew}
